22e20ddfc843472efe6d6d8a503f3c55f9e1480e
yelongtian <114457094+yelongtian@users.noreply.github.com>
2026-04-26 12:33:35 +0800

    Update device configuration for rk3399_emb-3531

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -11,27 +11,29 @@
 # 移植RK3399示例，其他RK3399可模仿
 # ============================================================================================================
 # 增加设备
-echo -e "\\ndefine Device/tvi_tvi3315a
-  DEVICE_VENDOR := Tvi
-  DEVICE_MODEL := TVI3315A
+echo -e "\\ndefine Device/rk3399_emb-3531
+  DEVICE_VENDOR := Rockchip
+  DEVICE_MODEL := EMB-3531
   SOC := rk3399
-  UBOOT_DEVICE_NAME := tvi3315a-rk3399
+  UBOOT_DEVICE_NAME := emb-3531-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169 -urngd
 endef
-TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
+TARGET_DEVICES += rk3399_emb-3531" >> target/linux/rockchip/image/armv8.mk
 
 # 替换package/boot/uboot-rockchip/Makefile
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
 
 # 复制dts与配置文件到package/boot/uboot-rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-emb-3531.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-emb-3531-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/emb-3531-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-emb-3531.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
 # 添加dtb补丁到target/linux/rockchip/patches-6.6
-cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
+cp -f $GITHUB_WORKSPACE/configfiles/patch/801-add-rk3399-emb-3531-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
 # ============================================================================================================
 # RK3399示例结束
 # ============================================================================================================
